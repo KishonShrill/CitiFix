ad530c0142a644403b9e594bc04fa2da58d21590
KishonShrill <crystalbluew@gmail.com>
2026-09-25 00:31:41 +0800

    chore(seo): adding default metatags on layout and reportid page

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,19 +4,39 @@ import "./globals.css";
 import { Providers } from "./providers";
 
 export const metadata: Metadata = {
-  title: "BetterIligan - Report Infrastructure Issues",
-  description: "Civic reporting platform for Iligan City infrastructure issues",
+    metadataBase: new URL("https://citifix.betteriligancity.org"),
+    title: {
+        default: "CitiFIX BetterIligan - Report Infrastructure Issues",
+        template: "%s | CitiFIX BetterIligan",
+    },
+    description: "Civic reporting platform for Iligan City infrastructure issues",
+    openGraph: {
+        siteName: "CitiFIX BetterIligan",
+        locale: "en_PH",
+        type: "website",
+        url: "https://citifix.betteriligancity.org",
+    },
+    twitter: {
+        card: "summary_large_image",
+    },
+    facebook: {
+        appId: "1036061575471786",
+    },
+    other: {
+        "fb:app_id": "1036061575471786",
+    },
+
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <html lang="en">
-      <body>
-        <Providers>
-          {children}
-        </Providers>
-        <Toaster position="bottom-right" richColors />
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body>
+                <Providers>
+                    {children}
+                </Providers>
+                <Toaster position="bottom-right" richColors />
+            </body>
+        </html>
+    );
 }
